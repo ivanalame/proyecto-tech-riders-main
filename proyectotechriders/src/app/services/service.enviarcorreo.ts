acae6420e4dae30909504bed3e79f7b0818ed194
ivanalame <alumno.399451@ies-azarquiel.es>
2024-05-15 20:27:11 +0200

    final

--- proyectotechriders/src/app/services/service.enviarcorreo.ts
+++ proyectotechriders/src/app/services/service.enviarcorreo.ts
@@ -8,7 +8,7 @@ export class Serviceenviarcorreo {
   constructor(private _http: HttpClient) {}
 
 enviarCorreoContrasena(email: string, asunto: string, mensaje: string): Observable<any> {
-    let url = "https://prod-68.westeurope.logic.azure.com/workflows/6f3ac4f7e8374048a7d2aec97ed52258/triggers/When_a_HTTP_request_is_received/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2FWhen_a_HTTP_request_is_received%2Frun&sv=1.0&sig=Nl_dU9kPxGGItdXAeHBm8jt_8U0gnY54XuVMJX-FIqY";
+    let url =environment.ulrLogicApp;
     let json = JSON.stringify({
       email: email,
       asunto: asunto,
--- proyectotechriders/src/environments/environment.development.ts
+++ proyectotechriders/src/environments/environment.development.ts
@@ -1,5 +1,7 @@
 export const environment = {
   urlApi: 'https://apitechriders.azurewebsites.net/',
   urlApiLocal: 'https://localhost:7000/',
+  ulrLogicApp:"https://prod-68.westeurope.logic.azure.com/workflows/6f3ac4f7e8374048a7d2aec97ed52258/triggers/When_a_HTTP_request_is_received/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2FWhen_a_HTTP_request_is_received%2Frun&sv=1.0&sig=Nl_dU9kPxGGItdXAeHBm8jt_8U0gnY54XuVMJX-FIqY",
+
   idUsuario:0
 };
